af796e24a4898cdff8e61295fa863ae890b05de7
ibnshayed <emranibnshayed@gmail.com>
2021-10-05 23:51:10 +0600

    adding jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
+	mode: 'jit',
 	purge: [
-		'./src/**/*.html',
-		'./src/**/*.js',
+		'./**/*.html',
+		'./src/**/*.{js,jsx,ts,tsx,vue}',
 	],
   darkMode: false, // or 'media' or 'class'
   theme: {
